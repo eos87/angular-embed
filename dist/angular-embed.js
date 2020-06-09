e4c2b45a7413b2115ac407da0d0c4f1d650aea81
Helmy Giacoman <helmygb@gmail.com>
2020-06-09 11:56:34 +0200

    LBSD-785 Changing iframely endpoint approach

--- dist/angular-embed.js
+++ dist/angular-embed.js
@@ -409,16 +409,21 @@
     'use strict';
     function IframelyProvider() {
         var provider = this;
+        var iframelyEndpoint = 'iframely'
+
+        // user iframely endpoint as default
+        provider.apiEndpoint = iframelyEndpoint;
+
         // register the service in the provider and inject dependencies
         function iframelyService($resource) {
             return {
-                embed: function(url, oEmbed) {
-                    var mode = oEmbed === true ? 'oembed': 'iframely';
+                embed: function(url) {
+                    var endpoint = provider.getEndpoint();
                     var apiKey = provider.getKey();
-                    var resource = $resource('https://iframe.ly/api/'+mode+'?api_key='+apiKey+'&url='+url);
+                    var resource = $resource('https://iframe.ly/api/'+endpoint+'?api_key='+apiKey+'&url='+url);
 
                     return resource.get().$promise.then(function(data) {
-                        if (mode !== 'oembed') {
+                        if (endpoint === iframelyEndpoint) {
                             // mimic oembed
                             data.title = data.meta.title;
                             data.provider_name = data.meta.site;
@@ -436,6 +441,15 @@
             },
             getKey: function() {
                 return provider.key;
+            },
+            getEndpoint: function() {
+                return provider.apiEndpoint;
+            },
+            useOembed: function() {
+                provider.apiEndpoint = 'oembed';
+            },
+            useIframely: function() {
+                provider.apiEndpoint = iframelyEndpoint;
             }
         });
     }
